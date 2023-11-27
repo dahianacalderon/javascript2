7b1b62f320d59ab61cbcacb74f340d5251583708
dahianacalderon <140189369+dahianacalderon@users.noreply.github.com>
2023-11-27 08:56:19 -0500

    js
    js

---
+++ Contador/contador.js
@@ -0,0 +1,32 @@
+window.addEventListener("keydown",(e)=>{
+    if(e.key=="+"){
+        incremento()
+    }
+    else if(e.key=="-"){
+        decremento()
+    }
+})
+
+let contador =0;
+const valor= document.getElementById("valor")
+function incremento(){
+    //alert("incremento")
+    contador+=1;
+    //localStorage.setItem("contador",contador)
+    valor.innerHTML=contador;
+}
+function decremento(){
+    //alerr("incremento")
+    if(contador>0){
+        contador-=1;
+        valor.innerHTML=contador;
+    }else{
+        contador=0;
+        valor.innerHTML=contador;
+        }
+
+}
+function resetear(){
+    contador=0;
+    valor.innerHTML=contador;
+}
